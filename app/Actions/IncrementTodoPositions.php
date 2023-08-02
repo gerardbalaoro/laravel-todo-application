50cbed90b313bad8c88ba8455eb9abf6ed86c0d0
Gerard Balaoro <miogerard@yahoo.com>
2023-08-03 03:50:37 +0800

    refactor: catch increment errors

--- app/Actions/IncrementTodoPositions.php
+++ app/Actions/IncrementTodoPositions.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use App\Enums\ApplicationError;
 use App\Models\Todo;
 
 /**
@@ -17,7 +18,11 @@ class IncrementTodoPositions
 
     public function run()
     {
-        Todo::where('position', '>=', $this->position)
-            ->increment('position', $this->increment);
+        try {
+            Todo::where('position', '>=', $this->position)
+                ->increment('position', $this->increment);
+        } catch (\Exception $e) {
+            throw ApplicationError::TODO_MOVE_NEXT_FAILED()->exception();
+        }
     }
 }
