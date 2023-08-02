7e5ee95bf3610ef88b17d5d199415f48bf11967f
Gerard Balaoro <miogerard@yahoo.com>
2023-08-03 01:34:27 +0800

    chore: remove unused controller methods

--- app/Http/Controllers/Api/V1/TodoController.php
+++ app/Http/Controllers/Api/V1/TodoController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class TodoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * List all todos.
      */
     public function index()
     {
@@ -19,47 +19,24 @@ class TodoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Create new todo.
      */
-    public function create(CreateTodoRequest $request)
-    {
-        $input = $request->safe()->collect();
-        $todo = Todo::create([
-            'name' => $input->get('name'),
-            'order' => $input->get('order'),
-        ]);
-
-        $todo->save();
-
-        return new TodoResource($todo);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(CreateTodoRequest $request)
     {
         //
     }
 
     /**
-     * Display the specified resource.
+     * Show specified todo.
      */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified todo.
      */
     public function update(Request $request, string $id)
     {
@@ -67,7 +44,7 @@ class TodoController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Delete the specified todo.
      */
     public function destroy(string $id)
     {
