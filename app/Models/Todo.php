6918b928379236b76abfdb8e553ed54467de080f
Gerard Balaoro <miogerard@yahoo.com>
2023-08-03 00:20:45 +0800

    chore: add todo migration and model

---
+++ app/Models/Todo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $content
+ * @property int $order
+ * @property bool $is_done
+ */
+class Todo extends Model
+{
+    use HasFactory;
+}
